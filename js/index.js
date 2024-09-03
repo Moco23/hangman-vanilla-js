dd40e52838d7a97e20bbf11d11c0dae9b5f0cee4
Momcilo Nikolic <momcilo.pos@gmail.com>
2024-09-03 19:49:10 +0200

    create start game function

--- js/index.js
+++ js/index.js
@@ -6,3 +6,24 @@ let errors = 0;
 let maxErrors = 6;
 let uniqueChars = new Set();
 let startTime;
+
+// start game function
+function startGame() {
+  // get the username
+  const username = document.querySelector("#username").value.traim();
+
+  // set if statmen
+  if (!username) {
+    alert("Please fill the name!");
+    return;
+  }
+
+  // get and set style on name screen
+  document.querySelector("#name-screen").style.display = "none";
+  // get and set style on game screen
+  document.querySelector("#game-screen").style.display = "block";
+
+  // fetch quote
+  fetchQuote();
+  startTime = Date.now();
+}
